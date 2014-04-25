f7d334d08746e40015cd0dee5768e44b9d9dbbcb
nicholas818 <cyberchase818@aim.com>
2014-04-24 17:16:55 -0700

    Added Google Analytics.

--- main.py
+++ main.py
@@ -117,6 +117,16 @@ page_template = """
 			button.parentNode.removeChild(button);	
 		}
 	</script>
+	
+	<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-50387911-1', 'brady-vs-grey.appspot.com');
+      ga('send', 'pageview');
+	</script>
 </head>
 <body>
 <font size="5">Q: How many videos has Brady Haran released since C.G.P. Grey last released a video?<br />
